bb10dbd2f2aae0874f04b4dd5371898c4ca184c5
sunnymodi29 <modisunny25@gmail.com>
2024-01-28 15:02:24 +0530

    Initial Commit

---
+++ src/Main.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const Main = () => {
+
+    const [inputValue, setInputValue] = useState("");
+
+    const display = (value) => {
+
+        setInputValue(inputValue + value);
+    }
+
+    const calculate = () => {
+        setInputValue(eval(inputValue));
+    }
+
+    const clear = () => {
+        setInputValue("");
+    }
+
+    return (
+        <form name='calc' className='calculator'>
+            <input type="text" className='value' value={inputValue} />
+            <span className="num clear" onClick={clear}>C</span>
+            <span onClick={() => display("/")}>/</span>
+            <span onClick={() => display("*")}>*</span>
+            <span onClick={() => display("7")}>7</span>
+            <span onClick={() => display("8")}>8</span>
+            <span onClick={() => display("9")}>9</span>
+            <span onClick={() => display("-")}>-</span>
+            <span onClick={() => display("4")}>4</span>
+            <span onClick={() => display("5")}>5</span>
+            <span onClick={() => display("6")}>6</span>
+            <span onClick={() => {display("+")}} className="plus">+</span>
+            <span onClick={() => display("1")}>1</span>
+            <span onClick={() => display("2")}>2</span>
+            <span onClick={() => display("3")}>3</span>
+            <span onClick={() => display("0")}>0</span>
+            <span onClick={() => display("00")}>00</span>
+            <span onClick={() => display(".")}>.</span>
+            <span onClick={() => calculate()} className="equal">=</span>
+        </form>
+    )
+}
+
+export default Main
